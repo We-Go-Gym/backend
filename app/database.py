eac9aa3cb41efe1db20cfcc347975382db09b834
IanLucca12 <89036876+IanLucca12@users.noreply.github.com>
2025-11-18 19:18:08 -0300

    feat: adicionar suporte a options do frontend e trocar banco

--- app/database.py
+++ app/database.py
@@ -9,17 +9,17 @@ from sqlalchemy.orm import sessionmaker
 
 
 #OBS: Está no modo SQLITE para ativar o mysql para o docker comente a l-7 e descomente da 8 até a 17
-engine = create_engine("sqlite:///fastapidb.db")
-# DB_USER = os.getenv("DB_USER", "root")
-# DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-# DB_HOST = os.getenv("DB_HOST", "localhost")
-# DB_PORT = os.getenv("DB_PORT", "3306")
-# DB_NAME = os.getenv("DB_NAME", "wggdb")
+#engine = create_engine("sqlite:///fastapidb.db")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "wggdb")
 
 # #Chama a base de dados
-# SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
--- app/main.py
+++ app/main.py
@@ -2,15 +2,25 @@
 # pylint: disable=unused-import
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
-from app.models import Aluno,Treino,Imc, Exercicio
-from app.routes import rota_aluno,rota_treino,rota_imc , rota_exercicio
+from app.models import Aluno, Treino, Imc, Exercicio
+from app.routes import rota_aluno, rota_treino, rota_imc, rota_exercicio
 
+# Cria as tabelas no banco
 Base.metadata.create_all(engine)
 
 app = FastAPI(
     title="API de Alunos e Fitness",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
